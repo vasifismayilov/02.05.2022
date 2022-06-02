cb73d1b936810157d96f71c80a1e61edf8418089
vasifismayilov <ismayilov2265@gmail.com>
2022-06-02 13:25:03 +0400

    22.05.2022

---
+++ homew34.py
@@ -0,0 +1,5 @@
+#1. b'\xff\xfef\x00e\x00r\x00m\x00a\x00' bu bytes datasını UTF-16 ilə decode edib, ekranda göstərin.
+# data = b'\xff\xfef\x00e\x00r\x00m\x00a\x00'
+# 
+# print(data.decode(encoding='utf-16', errors='strict'))
+
